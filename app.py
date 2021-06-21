7c751cd1ac72a4368502b05b8678cdd8bceb9ff8
schate <Sameer_Chate@intuit.com>
2021-06-20 23:52:55 -0700

    Removed matplotlib import

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import numpy as np
 from dash.dependencies import Input, Output, State
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import datetime
 import calendar
 # model & metric imports
